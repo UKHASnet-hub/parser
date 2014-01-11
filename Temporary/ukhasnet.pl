51e0c5907184229104b4ed9cbf85929aba209dd3
Mike Axford <mfaxford@gmail.com>
2014-01-11 17:58:19 +0000

    Added time test to findPacket query and supress output of blank lines in input data

--- Temporary/ukhasnet.pl
+++ Temporary/ukhasnet.pl
@@ -46,14 +46,25 @@ while ($loop){
         # Check we're connected to the DB
         if ($dbh){
                 # Prep SQL Statements
-		my $findPacket=$dbh->prepare("select packet.id as packetid from packet left join packet_rx on packet.id=packet_rx.packetid where origin=? and sequence=? and checksum = ?");
+		#my $findPacket=$dbh->prepare("select distinct packet.id as packetid from packet left join packet_rx on packet.id=packet_rx.packetid where origin=? and sequence=? and checksum = ?");
 			# TODO Add time check
 			# time between min - 15s and max +15s
+			my $findPacket=$dbh->prepare("
+				select packet.id as packetid from packet where 
+				origin= ? and sequence= ? and checksum = ? and from_unixtime(?)
+				between 
+					date_sub((select min(packet_rx_time) from packet_rx where packetid=packet.id), interval 1 minute)
+				and
+					date_add((select max(packet_rx_time) from packet_rx where packetid=packet.id), interval 1 minute)");
 		my $addPacket=$dbh->prepare("insert into packet (origin, sequence, checksum) values (?, ?, ?)");
 		my $addPacketRX=$dbh->prepare("insert into packet_rx (packetid, gateway, packet_rx_time) values (?, ?, from_unixtime(?))");
 		my $addRawPacket=$dbh->prepare("insert into raw_packet (packet_rx_id, data) values (?, ?)");
 
 		my $data=get($Options{'data_url'});
+
+		# TODO Need to skip lines we've already processed
+		# select max(time) from packet_rx where gw=JC
+
 		my $inline=0;
 		foreach (split('\n',$data)){
 			my $packet=$_;
@@ -87,32 +98,38 @@ while ($loop){
 				
 				# TODO Check if we have a recently matching packet
 				my $PacketID=0;
-				$findPacket->execute($origin, $seq, $csum);
-				if ($findPacket->rows()>=1){
-					while (my $row=$findPacket->fetchrow_hashref){
-						my $id=$row->{'packetid'};
-						print "-->Potential ID $id\n";
-						$PacketID=$row->{'packetid'};
-					}
-					# How to deal with more than one result ?
+				#$findPacket->execute($origin, $seq, $csum);
+				$findPacket->execute($origin, $seq, $csum, $time);
+				if ($findPacket->rows()==1){				# Single Match - GOOD
+					my $row=$findPacket->fetchrow_hashref;
+					$PacketID=$row->{'packetid'};
 
-					# Got a match
 
+					# TODO Process packet data
 
 					# Packet Data
+				} elsif ($findPacket->rows()>1){			# Multiple Matches - Bad
+					print "Error: Input line $packet matches more than one packet in DB\n";
+					while (my $row=$findPacket->fetchrow_hashref){
+						print "-->Potential ID " . $row->{'packetid'} . "\n";
+					}
 				} else {
 					$addPacket->execute($origin,$seq,$csum);
 					$PacketID=$addPacket->{mysql_insertid};
 				}
 
-				if ($PacketID !=0){
+				if ($PacketID > 0){
 					$addPacketRX->execute($PacketID, $gw, $time);	## TODO Need to convert time
 					my $rxID=$addPacketRX->{mysql_insertid};
 					$addRawPacket->execute($rxID,$packet);
 					# Path
 				}
 
-				last if ($inline >=5);
+				#last if ($inline >=5);
+			} elsif (/^(.*)?[rt]x: $/) { 			# jcoxon Dump format
+				# Surpress blank lines
+			} elsif (/^(.*)?Stop$/) {			# jcoxon Dump format
+				# Surpress blank lines
 			} else {
 				print "?> $_\n";
 			}
