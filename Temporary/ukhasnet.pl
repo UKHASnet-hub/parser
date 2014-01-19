9720bb62f3dcb439fd0ec4855da8f4db9236f02d
Mike Axford <mfaxford@gmail.com>
2014-01-19 15:46:27 +0000

    Added Transations, Uses originid in packet table

--- Temporary/ukhasnet.pl
+++ Temporary/ukhasnet.pl
@@ -6,6 +6,15 @@ use DBI();
 use POSIX qw(setsid);
 use Sys::Syslog qw(:DEFAULT setlogsock);
 
+# TODO List
+# Test Transactions work (i.e. if a query fails we roll it all back) - How to test?
+# Path table links to nodes rather than storing the path values.
+# Store data from the packets into the DB
+# Log to syslog instead of console
+# How to deal with sequence = a
+#   Should also not link to previous packets (requires us to remember state)
+# Get password from command line
+
 my %Options=(
   db_host     =>"philcrump.co.uk",
   db_database =>"postgres",
@@ -15,7 +24,6 @@ my %Options=(
   sleep_time  =>3,
   lock_file   =>$ENV{"HOME"} . "/run/ukhasnet.pid"
 );
-
 
 if  ( -f $Options{'lock_file'}){
         print "A process is already running\n";
@@ -48,7 +56,7 @@ while ($loop){
         # Check we're connected to the DB
         if ($dbh){
                 # Prep SQL Statements
-		my $getData=$dbh->prepare("select id, nodeid, extract(epoch from time) as time, packet from ukhasnet.upload where state='Pending' limit 10");
+		my $getData=$dbh->prepare("select id, nodeid, extract(epoch from time) as time, packet from ukhasnet.upload where state='Pending' limit 50");
 		my $findPacket=$dbh->prepare("
 			select packet.id as packetid from packet where
 			origin=? and sequence=? and checksum=? and to_timestamp(?)
@@ -56,17 +64,24 @@ while ($loop){
 				(select min(packet_rx_time) from packet_rx where packetid=packet.id) - interval '1' minute
 			and
 				(select max(packet_rx_time) from packet_rx where packetid=packet.id) + interval '1' minute");
-		my $addPacket=$dbh->prepare("insert into packet (origin, sequence, checksum) values (?, ?, ?)");
+		my $addPacket=$dbh->prepare("insert into packet (origin, originid, sequence, checksum) values (?, ?, ?, ?)");
 		my $addPacketRX=$dbh->prepare("insert into packet_rx (packetid, gatewayid, packet_rx_time, uploadid) values (?, ?, to_timestamp(?), ?)");
 		my $addPath=$dbh->prepare("insert into path (packet_rx_id, position, node) values (?, ?, ?)");
 		my $uploadUpdate=$dbh->prepare("update upload set packetid=?, state='Processed' where id=?");
 		my $uploadFailed=$dbh->prepare("update upload set state='Error' where id=?");
 
+		my $getNode=$dbh->prepare("select id from nodes where name = ?");
+		my $addNode=$dbh->prepare("insert into nodes (name) values (?)");
+
 		$getData->execute();
 		while (my $record=$getData->fetchrow_hashref){
+
 			$entries++;
 			print ">".$record->{'packet'}."\t(".$record->{'id'}.")\t".$record->{'time'}."\n";
-			if ($record->{'packet'} =~ /^([0-9])([a-z])([A-Z0-9\.,\-]+)\[([A-Za-z,]+)\]$/){
+			if ($record->{'packet'} =~ /^([0-9])([a-z])([A-Z0-9\.,\-]+)\[([A-Za-z,]+)\]\r?$/){
+				$dbh->begin_work();	# Start Transaction
+				my $error=0;
+
 				#my $repeat=$1;
 				my $seq=$2;
 				my $data=$3;
@@ -77,23 +92,42 @@ while ($loop){
 
 				print "->".$record->{'packet'}."\t(".$record->{'time'}.")\t$seq\t$data\t$origin($csum)\t$path\n";
 
-				# TODO Add in Transactions ?
-
 				my $PacketID=0;
 				$findPacket->execute($origin, $seq, $csum, $record->{'time'});
 				if ($findPacket->rows()==1){				# Single Match - GOOD
 					my $row=$findPacket->fetchrow_hashref;
 					$PacketID=$row->{'packetid'};
 				} elsif ($findPacket->rows()>1){			# Multiple Matches - Bad
-					print "Error: Input line ".$record->{'packet'}." matches more than one packet in DB\n";
+					$error=1;
+					print "Error: Input line ".$record->{'packet'}."(".$record->{'id'}.") matches more than one packet in DB\n";
 					while (my $row=$findPacket->fetchrow_hashref){
 						print "-->Potential ID " . $row->{'packetid'} . "\n";
 					}
 				} else {
-					$addPacket->execute($origin,$seq,$csum);
-					$PacketID=$dbh->last_insert_id(undef, "ukhasnet", "packet", undef);
+					## Get NodeID for Origin
+					my $nodeID=0;
+					$getNode->execute($origin);
+					if ($getNode->rows()==1){
+						my $nodeRow=$getNode->fetchrow_hashref;
+						$nodeID=$nodeRow->{'id'};
+					} elsif ($getNode->rows()>1){
+						$error=1;
+						print "Error: Input line ".$record->{'packet'}."(".$record->{'id'}.") matches more than one origin node in DB\n";
+						while (my $nodeRow=$getNode->fetchrow_hashref){
+							print "-->Potential ID " . $nodeRow->{'id'} . "\n";
+						}
+					} else {
+						$addNode->execute($origin);
+						$nodeID=$dbh->last_insert_id(undef, "ukhasnet", "nodes", undef);
+						print "--> Adding Node $origin($nodeID)\n";
+					}
+						
+					if ($nodeID >0){
+						$addPacket->execute($origin,$nodeID,$seq,$csum);
+						$PacketID=$dbh->last_insert_id(undef, "ukhasnet", "packet", undef);
+						# TODO Process packet data
+					}
 
-					# TODO Process packet data
 				}
 
 				if ($PacketID > 0){
@@ -106,18 +140,21 @@ while ($loop){
 						$addPath->execute($rxID, $hop++, $_);
 					}
 				}
+				if ($error == 0){
+					$dbh->commit();
+				} else {
+					# Rollback
+					die "DB Transaction failed\n";
+					$dbh->rollback();
+					$uploadFailed->execute($record->{'id'});
+				}
 			} else {
 				print "?> ".$record->{'packet'}."(".$record->{'id'}.")\n";
 				$uploadFailed->execute($record->{'id'});
 			}
 
-			# If previous sequence from this node was A ignore any other sequenct
-			# Search for packet in DB
-			# Sequence of A signifies a reboot - do something different ?
-			# If not found add packet
 
 		}
-
 
 		# Close SQL Connections
 		$getData->finish();
@@ -127,7 +164,9 @@ while ($loop){
 		$addPath->finish();
 		$uploadUpdate->finish();
 		$uploadFailed->finish();
-                $dbh->disconnect();
+		$getNode->finish();
+		$addNode->finish();
+		$dbh->disconnect();
 		print "Done\n";
 		sleep (60) if ($loop && ($entries==0));
 		sleep (10) if $loop;
@@ -152,5 +191,6 @@ sub daemonize {
 sub catch_hup {
         $loop=0;
         syslog('warning', 'Got HUP');
+	print "Got HUP\n";
 }
 
