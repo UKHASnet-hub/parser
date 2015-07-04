e9d88576ef5dda36b9a3821ffaa5cd659bc1af79
Mike Axford <m1ari@m1ari.co.uk>
2015-07-04 11:31:57 +0000

    fixed typo :#2

--- ukhasnet.pl
+++ ukhasnet.pl
@@ -197,7 +197,7 @@ while ($loop){
 						if ($type->{'type'} eq "Float"){ 
 							my $p=0;
 							foreach my $v (split(/,/, $val)){
-								if ($v ~= /[0-9]+\.?[0-9]*/){
+								if ($v =~ /[0-9]+\.?[0-9]*/){
 									$data_float->execute($datarow->{'packetid'}, $type->{'id'}, $v, $p++);
 								} else {
 									syslog('error', "Error processing type ".$type->{'id'}." with value $v (".$datarow->{'packetid'}.")");
